dcad5736f082c077151225606804ea121b0ac17b
TyrantCerberus <53711771+TyrantCerberus@users.noreply.github.com>
2025-01-22 15:05:32 +0000

    Pref selection widget for quirk.

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/iris/handedness.tsx
@@ -0,0 +1,7 @@
+import { FeatureChoiced } from '../base';
+import { FeatureDropdownInput } from '../dropdowns';
+
+export const handedness: FeatureChoiced = {
+  name: 'Handedness',
+  component: FeatureDropdownInput,
+};
