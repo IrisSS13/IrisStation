9caec9154efb6172e21df309773cb31b195bb822
TyrantCerberus <53711771+TyrantCerberus@users.noreply.github.com>
2025-02-16 18:32:55 +0000

    UI file for miscreant pref.

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/miscreant.ts
@@ -0,0 +1,21 @@
+// IRIS STATION FILE - MISCREANTS_ANTAG
+
+import { Antagonist, Category } from '../base';
+
+export const MISCREANT_MECHANICAL_DESCRIPTION = `
+      A non-conversion team antag with RP-centric objectives. Expect highly variable amounts of conflict as it will depend on your team's objectives and your approach and may range from minimal to extreme.
+   `;
+
+const Miscreant: Antagonist = {
+  key: 'miscreant',
+  name: 'Miscreant',
+  description: [
+    `
+      Become a miscreant and work with your fellow miscreants to accomplish your goal!
+    `,
+    MISCREANT_MECHANICAL_DESCRIPTION,
+  ],
+  category: Category.Roundstart,
+};
+
+export default Miscreant;
