84cb7d68cc0916d40d0557a090bc5a97bcb06f81
synthtwo <127706731+SynthTwo@users.noreply.github.com>
2025-02-25 02:11:26 +0000

    clarification

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/nova/antag_optin.tsx
@@ -6,7 +6,7 @@ export const antag_opt_in_status_pref: FeatureChoiced = {
   name: 'Be Antagonist Target',
   description:
     'This is for objective targetting and OOC consent.\
-    By extension, picking "Round Remove" will allow you to be round removed in applicable situations, even by non-antagonists. \
+    By picking Round Remove, it means that you are okay with potential round-removal by antagonists. \
     Enabling any non-ghost antags \
     (revenant, abductor contractor, etc.) will force your opt-in to be, \
     at minimum, "Temporarily Inconvenience".',
