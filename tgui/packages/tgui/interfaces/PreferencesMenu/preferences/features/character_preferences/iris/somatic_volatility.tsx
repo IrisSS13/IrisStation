03d85141e4bd2786a5c9dc2506af041503807b09
synthtwo <127706731+SynthTwo@users.noreply.github.com>
2025-04-17 03:20:45 +0100

    tsx file

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/iris/somatic_volatility.tsx
@@ -0,0 +1,7 @@
+import { FeatureChoiced } from '../../base';
+import { FeatureDropdownInput } from '../../dropdowns';
+
+export const somatic_volatility_gib_choice: FeatureChoiced = {
+  name: 'Somatic Volatility',
+  component: FeatureDropdownInput,
+};
