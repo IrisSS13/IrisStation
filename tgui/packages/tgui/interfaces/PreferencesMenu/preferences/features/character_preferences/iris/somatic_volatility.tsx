162b076c867cf021a487d7a5ce8bf55b3a27b4c1
TyrantCerberus <53711771+TyrantCerberus@users.noreply.github.com>
2025-04-21 13:33:58 +0000

    Update somatic_volatility.tsx
    Changes the name of the dropdown in the quirk UI to better communicate what is being chosen.

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/iris/somatic_volatility.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/iris/somatic_volatility.tsx
@@ -2,6 +2,6 @@ import { FeatureChoiced } from '../../base';
 import { FeatureDropdownInput } from '../../dropdowns';
 
 export const somatic_volatility_gib_choice: FeatureChoiced = {
-  name: 'Somatic Volatility',
+  name: 'Gib Style',
   component: FeatureDropdownInput,
 };
