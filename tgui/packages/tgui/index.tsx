0525d70dc7e91184e3c57043c8e8dafa00b5a8f6
synthtwo <127706731+SynthTwo@users.noreply.github.com>
2025-05-24 15:14:32 +0100

    lalalaa

--- tgui/packages/tgui/index.tsx
+++ tgui/packages/tgui/index.tsx
@@ -53,6 +53,12 @@ function setupApp() {
   setGlobalStore(store);
 
   setupGlobalEvents();
+  setupHotKeys({
+    keyUpVerb: 'KeyUp',
+    keyDownVerb: 'KeyDown',
+    // In the future you could send a winget here to get mousepos/size from the map here if it's necessary
+    verbParamsFn: (verb, key) => `${verb} "${key}" 0 0 0 0`,
+  });
   setupHotKeys();
   captureExternalLinks();
 
