0685e34c159dacb0fc693c5aae9099dbe3eb2cbd
synthtwo <127706731+SynthTwo@users.noreply.github.com>
2025-05-25 15:47:27 +0100

    backend

--- tgui/packages/tgui/backend.ts
+++ tgui/packages/tgui/backend.ts
@@ -13,6 +13,11 @@
 
 import { perf } from 'common/perf';
 import { createAction } from 'common/redux';
+import {
+  releaseHeldKeys,
+  startKeyPassthrough,
+  stopKeyPassthrough,
+} from 'tgui-core/hotkeys';
 import { BooleanLike } from 'tgui-core/react';
 
 import { setupDrag } from './drag';
@@ -212,6 +217,8 @@ export const backendMiddleware = (store) => {
       Byond.winset(Byond.windowId, {
         'is-visible': false,
       });
+      stopKeyPassthrough();
+      releaseHeldKeys();
       setTimeout(() => focusMap());
     }
 
@@ -238,6 +245,7 @@ export const backendMiddleware = (store) => {
       logger.log('backend/update', payload);
       // Signal renderer that we have resumed
       resumeRenderer();
+      startKeyPassthrough();
       // Setup drag
       setupDrag();
       // We schedule this for the next tick here because resizing and unhiding
